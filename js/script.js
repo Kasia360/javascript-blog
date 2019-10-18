ffb345e9c08938c894e49fbed6d0608e7b8570a6
Kasia Brzezinska <kasiaplaskota4@gmail.com>
2019-10-19 00:22:11 +0200

    fill calculateTagClass with functions

--- js/script.js
+++ js/script.js
@@ -118,6 +118,11 @@ if(tags[tag] < params.min){
 }
 
 function calculateTagClass(count, params){
+   const normalizedCount = count - params.min;
+   const normalizedMax = params.max - params.min;
+   const percentage = normalizedCount / normalizedMax;
+   const classNumber = Math.floor( percentage * (optCloudClassCount - 1) + 1 );
+   return classNumber;
 
 }
 
